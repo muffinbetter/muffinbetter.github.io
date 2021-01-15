6a807f8d84344b4d664cd8d67a3e0fd8980ef1c5
muffinbetter <evekazarian@yahoo.com>
2021-01-15 14:07:35 -0500

    update email

--- assets/feedback_form.php
+++ assets/feedback_form.php
@@ -1,53 +1,9 @@
-<!-- <?php
 
-#Receive user input
-$email_address = $_POST['email_address'];
-$feedback = $_POST['feedback'];
-
-#Filter user input
-function filter_email_header($form_field) {  
-return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
-}
-
-$email_address  = filter_email_header($email_address);
-
-#Send email
-$headers = "From: $email_addressn";
-$sent = mail('eve.kazarian@gmail.com', 'Feedback Form Submission', $feedback, $headers);
-
-#Thank user or notify them of a problem
-if ($sent) {
-
-?><html>
-<head>
-<title>Thank You</title>
-</head>
-<body>
-<h1>Thank You</h1>
-<p>Thank you for your feedback.</p>
-</body>
-</html>
-<?php
-
-} else {
-
-?><html>
-<head>
-<title>Something went wrong</title>
-</head>
-<body>
-<h1>Something went wrong</h1>
-<p>We could not send your feedback. Please try again.</p>
-</body>
-</html>
-<?php
-}
-?> -->
 
 
 <?php
 if(isset($_POST["submit"])) {
-$recipient = "brentw.white@gmail.com"; //my email
+$recipient = "eve.kazarian@gmail.com"; //my email
 echo $subject = 'Email message from Point Plumbing';
 echo $name = $_POST ["yourName"];
 echo $email = $_POST["yourEmail"];
